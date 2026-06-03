9eac4464eb31f6afd347bdf1e1c999c2fac72549
KICHA\KISHORE KUMAR <kicha.sweet@gmail.com>
2026-06-03 11:07:02 +0530

    added new items

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
